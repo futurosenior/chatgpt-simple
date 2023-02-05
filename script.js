2bb83a5dabfca651b4ac48c17117365c8236b083
Tiago Oliveira Santos <tiago.santos@somosquiq.com>
2023-02-05 15:31:53 -0300

    feat: design with simple chatgpt integration

---
+++ script.js
@@ -0,0 +1,53 @@
+const inputPergunta = document.getElementById("inputPergunta");
+const resultadoIA = document.getElementById("resultadoIA");
+
+const OPENAI_API_KEY = "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";
+
+function EnviarPergunta() {
+	var valorPergunta = inputPergunta.value;
+
+  fetch("https://api.openai.com/v1/completions", {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json", 
+      Authorization: "Bearer " + OPENAI_API_KEY,
+    },
+    body: JSON.stringify({
+      model: "text-davinci-003",
+      prompt: valorPergunta,
+      max_tokens: 2048, // tamanho da resposta
+      temperature: 1, // criatividade na resposta
+    }),
+  })
+    .then((response) => response.json())
+    .then((json) => {
+      if (resultadoIA.value) resultadoIA.value += "\n";
+
+      if(json.error?.message){
+        resultadoIA.value += `Error: ${json.error.message}`;
+      } else if (json.choices?.[0].text) {
+        var text = json.choices[0].text || "Sem resposta";
+
+        resultadoIA.value += "Chat GPT: " + text;
+      }
+
+      resultadoIA.scrollTop = resultadoIA.scrollHeight;
+    })
+    .catch((error) => console.error("Error:", error))
+    .finally(() => {
+      inputPergunta.value = "";
+      inputPergunta.disabled = false;
+      inputPergunta.focus();
+    });
+
+	if (resultadoIA.value) resultadoIA.value += "\n\n\n";
+
+	resultadoIA.value += `Eu: ${valorPergunta}`;
+	inputPergunta.value = "Carregando...";
+	inputPergunta.disabled = true;
+
+  resultadoIA.scrollTop = resultadoIA.scrollHeight;
+}
+
+document.getElementById("EnviarPergunta").addEventListener("click", EnviarPergunta)
